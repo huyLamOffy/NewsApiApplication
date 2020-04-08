4af263468e2ad4f3a595b1f4aa1fd2d967bfaf9c
Huy Hoang Lam <huy.lam@okiela.com>
2020-04-08 14:43:29 +0700

    ADD: first initialization proj

---
+++ NewsApiApplication/NewsApiApplication/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  NewsApiApplication
+//
+//  Created by OkieLa Dev on 4/8/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    
+    }
+}
+
